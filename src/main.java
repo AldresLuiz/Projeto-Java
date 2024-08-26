9746e19f7d0a17625b48355081e6716276f2b0e5
AldresLuiz <filesaldres@gmail.com>
2024-08-26 18:31:25 -0300

    0.0.2V-ALPHA

--- src/main.java
+++ src/main.java
@@ -1,8 +1,29 @@
 package src;
+import src.window.engine;
 import src.window.windowconfig;
 
 public class main {
+
+    windowconfig window = new windowconfig();
+
     public static void main(String args[]){
-        windowconfig test = new windowconfig();
+        new main();
+    }
+
+    public main (){
+        startWindow();
+    }
+
+    public void startWindow(){
+        window.setDimension(800,600);
+        window.show(true);
+        window.Title = "Testando Classe";
+        window.create();
+        startgame();
+    }
+
+    public void startgame(){
+        engine test = new engine(window);
+        test.startGame();
     }
 }
--- src/window/windowconfig.java
+++ src/window/windowconfig.java
@@ -1,20 +1,39 @@
 package src.window;
 
 import javax.swing.*;
+import java.awt.*;
+
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class windowconfig {
+public class windowconfig extends JFrame {
 
     public String Title = "Home";
     private Integer Height = 500;
     private Integer Width = 500;
+    private Boolean isCreated = false;
 
     public windowconfig(){
-        JFrame wind = new JFrame();
-        wind.setTitle(Title);
-        wind.setSize(Width, Height);
-        wind.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        wind.setResizable(false);
-        wind.setVisible(true);
+        System.out.println("Window Config Class BOOTED.");
+    }
+
+    public void create(){
+        isCreated = true;
+        setSize(Width, Height);
+        setTitle(Title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void show(Boolean estado){
+        setVisible(estado);
+    }
+
+    public void setDimension(int Largura, int Altura){
+        Height = Altura;
+        Width = Largura;
+    }
+
+    @Override
+    public Graphics getGraphics() {
+        return super.getGraphics();
     }
 }
